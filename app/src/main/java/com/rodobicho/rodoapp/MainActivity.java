bb9390c36c4b5f6e50b0dcf28e5d0819544252ac
Arthur <arthurvieiraguimaraes@gmail.com>
2020-05-09 17:03:44 -0300

    Adicionado botao google login

--- app/src/main/java/com/rodobicho/rodoapp/MainActivity.java
+++ app/src/main/java/com/rodobicho/rodoapp/MainActivity.java
@@ -10,8 +10,18 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,11 +29,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageButton btn_novaocorrencia = (ImageButton)findViewById(R.id.btn_novaocorrencia);
-        ImageButton btn_entrar = (ImageButton)findViewById(R.id.btn_entrar);
-        ImageButton btn_visualizarocorrencias = (ImageButton)findViewById(R.id.btn_visualizarocorrencias);
-        ImageButton btn_sobre = (ImageButton)findViewById(R.id.btn_sobre);
-        ImageButton btn_cadastrar = (ImageButton)findViewById(R.id.btn_cadastrar);
+        ImageButton btn_novaocorrencia = (ImageButton) findViewById(R.id.btn_novaocorrencia);
+        ImageButton btn_entrar = (ImageButton) findViewById(R.id.btn_entrar);
+        ImageButton btn_visualizarocorrencias = (ImageButton) findViewById(R.id.btn_visualizarocorrencias);
+        ImageButton btn_sobre = (ImageButton) findViewById(R.id.btn_sobre);
+        ImageButton btn_cadastrar = (ImageButton) findViewById(R.id.btn_cadastrar);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        final GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Set the dimensions of the sign-in button.
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
 
         btn_novaocorrencia.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,6 +95,64 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
         }
 
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.sign_in_button:
+                        signIn(mGoogleSignInClient);
+                        break;
+                    // ...
+                }
+            }
+        });
+
+
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+    }
+
+    private void signIn(GoogleSignInClient mGoogleSignInClient) {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 201);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == 201) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            updateUI(null);
+        }
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
     }
 
     public static boolean hasPermissions(Context context, String... permissions) {
