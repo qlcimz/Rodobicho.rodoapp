690bf7bce6024c10aa0f67ba0eef4e695951dafa
Arthur <arthurvieiraguimaraes@gmail.com>
2020-05-16 17:54:15 -0300

    Add logout

--- app/src/main/java/com/rodobicho/rodoapp/MainActivity.java
+++ app/src/main/java/com/rodobicho/rodoapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.rodobicho.rodoapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -22,21 +23,23 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
     private SignInButton signInButton;
+    private Button btn_sair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btn_novaocorrencia = (Button) findViewById(R.id.btn_novaocorrencia);
-        Button btn_sair = (Button) findViewById(R.id.btn_sair);
+        btn_sair = (Button) findViewById(R.id.btn_sair);
         Button btn_visualizarocorrencias = (Button) findViewById(R.id.btn_visualizarocorrencias);
         Button btn_sobre = (Button) findViewById(R.id.btn_sobre);
-
+        btn_sair.setVisibility(View.GONE);
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -100,34 +103,45 @@ public class MainActivity extends AppCompatActivity {
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                switch (v.getId()) {
-                    case R.id.sign_in_button:
                         signIn(mGoogleSignInClient);
-                        break;
-                    // ...
-                }
             }
         });
 
+        btn_sair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut(mGoogleSignInClient);
+            }
+        });
 
     }
 
 
-//    @Override
-//    protected void onStart() {
-//        super.onStart();
-//        // Check for existing Google Sign In account, if the user is already signed in
-//        // the GoogleSignInAccount will be non-null.
-//        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-//        if(account!=null) {
-//            updateUI(false);
-//        }
-//
-//    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if(account!=null) {
+            updateUI(account);
+        }
+
+    }
 
     private void signIn(GoogleSignInClient mGoogleSignInClient) {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, 201);
+    }
+
+    private void signOut(GoogleSignInClient mGoogleSignInClient) {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        updateUI(null);
+                    }
+                });
     }
 
     @Override
@@ -148,19 +162,21 @@ public class MainActivity extends AppCompatActivity {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
 
             // Signed in successfully, show authenticated UI.
-            updateUI(true);
+            updateUI(account);
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            updateUI(false);
+            updateUI(null);
         }
     }
 
-    private void updateUI(boolean isLogin) {
-        if (isLogin) {
-            signInButton.setVisibility(View.GONE);
-        } else {
+    private void updateUI(GoogleSignInAccount account) {
+        if (account == null) {
             signInButton.setVisibility(View.VISIBLE);
+            btn_sair.setVisibility(View.GONE);
+        } else {
+            btn_sair.setVisibility(View.VISIBLE);
+            signInButton.setVisibility(View.GONE);
         }
     }
 
